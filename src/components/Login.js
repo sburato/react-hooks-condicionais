c7763d7a72ef84cf3b42d7d2efb3b2fe56a299b9
Silvano Burato <silvanoburato@gmail.com>
2020-11-23 22:58:42 -0300

    Commit inicial

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../styles.css";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+   
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/components/Titulo.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../styles.css";
+
+class Titulo extends Component {
+  render() {
+    return (
+      <header id="titulo">React - Aviso se o usuário está logado</header>
+    );
+  }
+}
+
+export default Titulo;
